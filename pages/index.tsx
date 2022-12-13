b321bbc7d1ca87965d388bfb6b01e2cb992254f1
Nayaabkhan Khan <nayaab@hey.com>
2022-12-13 12:43:14 +0530

    Add a simple reproduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,29 @@
+import React from 'react'
+import { LiveProvider, LivePreview, LiveError } from 'react-live'
+
+const code = `
+<input
+  type='text'
+  value={readState()}
+  onChange={(e) => writeState(e.target.value)}
+/>
+`.trim()
+
 export default function Home() {
-  return <h1>Hello world!</h1>
+  const [state, setState] = React.useState('')
+
+  function readState() {
+    return state
+  }
+
+  function writeState(newValue: string) {
+    setState(newValue)
+  }
+
+  return (
+    <LiveProvider code={code} scope={{ readState, writeState }}>
+      <LivePreview />
+      <LiveError />
+    </LiveProvider>
+  )
 }
